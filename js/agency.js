ec628245216a866085a9778f643a7233f892171c
Sheldon Pasciak <sheldon@pasciak.com>
2020-03-24 11:56:32 -0600

    test
    test

--- js/agency.js
+++ js/agency.js
@@ -1,8 +1,6 @@
 (function($) {
   "use strict"; // Start of use strict
 
-  console.log("ready....");
-
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
